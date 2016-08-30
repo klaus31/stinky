aca7d07cd74fdf31fd5077a697193a74c9b399e1
klaus31 <danieloltmanns@gmx.de>
2016-08-30 16:45:55 +0200

    add headline to tables

--- src/2/result/result.js
+++ src/2/result/result.js
@@ -5,6 +5,7 @@ var Result = function() {
   const TABLE_ROW_HEIGHT = 40;
   const TABLE_Y = 200;
   const HOLES_IN_PARKOUR = 18;
+  const CELL_PADDING = 5;
 
   this.preload = function() {
     game.load.image('table-row', 'result/table-row.png');
@@ -27,6 +28,24 @@ var Result = function() {
 
   this.create = function() {
     var rowPositions = getPosOfRows();
+    var createRow = function(x, y, cols, textstyle) {
+      game.add.text(x, y, cols[0], textstyle);
+      game.add.text(x + 180, y, cols[1], textstyle);
+      game.add.text(x+255, y, cols[2], textstyle);
+      game.add.text(x+325, y, cols[3], textstyle);
+    }
+    var createHeadlines = function() {
+      var textstyle = {
+        fontSize: '16px',
+        fill: '#000',
+        font: 'Courier',
+        fontWeight: 'bold'
+      };
+      var y = rowPositions[0].y - CELL_PADDING - 16;
+      var headlines = ['Hole', 'Par', 'Tries', 'Total'];
+      createRow(rowPositions[0].x + CELL_PADDING, y, headlines, textstyle);
+      createRow(rowPositions[9].x + CELL_PADDING, y, headlines, textstyle);
+    }
     var createTable = function() {
       var i = rowPositions.length;
       while (i--) {
@@ -34,6 +53,7 @@ var Result = function() {
       }
     }
     game.add.sprite(0, 0, 'result-bg');
+    createHeadlines();
     createTable();
   }
 
