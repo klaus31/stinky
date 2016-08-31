1a5ad2a1ae10dca0a49461f93ed7bbaab7fc3038
klaus31 <danieloltmanns@gmx.de>
2016-08-31 17:04:10 +0200

    use a atlas builder

---
+++ src/2/atlas-builder.js
@@ -0,0 +1,47 @@
+/**
+ * builder for atlas data of scale 1
+ */
+var AtlasBuilder = function(image, w, h) {
+
+  var meta = {
+    image: image,
+    size: {
+      w: w,
+      h: h
+    },
+    scale: 1,
+    format: 'RGBA8888'
+  };
+
+  var frames = {};
+
+  this.addFrame = function(name, x, y, w, h) {
+    frames[name] = {
+      frame: {
+        x: x,
+        y: y,
+        w: w,
+        h: h
+      },
+      rotated: false,
+      trimmed: false,
+      spriteSourceSize: {
+        x: 0,
+        y: 0,
+        w: w,
+        h: h
+      },
+      sourceSize: {
+        w: w,
+        h: h
+      }
+    }
+  }
+  
+  this.build = function() {
+    return {
+      meta: meta,
+      frames: frames
+    }
+  }
+};
--- src/2/mainmenu/main-menu.js
+++ src/2/mainmenu/main-menu.js
@@ -2,8 +2,13 @@ var MainMenu = function() {
 
   this.preload = function() {
     game.load.image('parkour-1', 'mainmenu/parkour-1.png');
+    var atlasBuilder = new AtlasBuilder('parkour-1-buttons', 222, 62);
+    atlasBuilder.addFrame('normal', 0, 0, 74, 62);
+    atlasBuilder.addFrame('hover', 74, 0, 74, 62);
+    atlasBuilder.addFrame('down', 148, 0, 74, 62);
+    game.load.atlas('parkour-1-buttons', 'mainmenu/parkour-1-buttons.png', null, atlasBuilder.build());
+
     game.load.image('parkour-2', 'mainmenu/parkour-2.png');
-    game.load.atlas('parkour-1-buttons', 'mainmenu/parkour-1-buttons.png', 'mainmenu/parkour-1-buttons.json');
   }
 
   this.create = function() {
