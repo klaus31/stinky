30568424f702fef4d4708a457b893a9310c26ec8
klaus31 <danieloltmanns@gmx.de>
2016-08-31 17:19:51 +0200

    add button to second toilet

--- src/2/mainmenu/main-menu.js
+++ src/2/mainmenu/main-menu.js
@@ -9,15 +9,27 @@ var MainMenu = function() {
     game.load.atlas('parkour-1-buttons', 'mainmenu/parkour-1-buttons.png', null, atlasBuilder.build());
 
     game.load.image('parkour-2', 'mainmenu/parkour-2.png');
+    var atlasBuilder = new AtlasBuilder('parkour-2-buttons', 138, 45);
+    atlasBuilder.addFrame('normal', 0, 0, 46, 45);
+    atlasBuilder.addFrame('hover', 46, 0, 46, 45);
+    atlasBuilder.addFrame('down', 92, 0, 46, 45);
+    game.load.atlas('parkour-2-buttons', 'mainmenu/parkour-2-buttons.png', null, atlasBuilder.build());
   }
 
   this.create = function() {
     game.add.sprite(0, 0, 'parkour-1');
     game.add.sprite(game.world.width / 2, 0, 'parkour-2');
     game.add.button(207, 71, 'parkour-1-buttons', startGame, game, 'hover', 'normal', 'down');
+    game.add.button(game.world.width / 2 + 220, 143, 'parkour-2-buttons', payFirst, game, 'hover', 'normal', 'down');
+
+
   }
 
   this.update = function() {}
+
+  var payFirst = function() {
+    window.alert('pay first');
+  }
 
   var startGame = function() {
     // TODO set data from pressed parkour button
